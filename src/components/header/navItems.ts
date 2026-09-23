edab3e9920b24119fc9396637768f936d648e420
cmorales-arsit <cmorales@arsit.com.ar>
2026-09-23 10:11:12 -0300

    Cambios

---
+++ src/components/header/navItems.ts
@@ -0,0 +1,31 @@
+interface NavItem {
+    nombre: string;
+    href: string;
+}
+
+export const navItems: NavItem[] = [
+    {
+        nombre: "Home",
+        href: "/",
+    },
+    {
+        nombre: "Soluciones",
+        href: "/soluciones",
+    },
+    {
+        nombre: "Industrias",
+        href: "#industrias",
+    },
+    {
+        nombre: "Consultoría",
+        href: "/consultoria",
+    },
+    {
+        nombre: "Nosotros",
+        href: "/nosotros/politicadecalidad",
+    },
+    {
+        nombre: "Contacto",
+        href: "/contacto",
+    },
+];
