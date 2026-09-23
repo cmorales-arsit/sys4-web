cbc566d567d74ca9e7dff268210f539fd2a08c35
cmorales-arsit <cmorales@arsit.com.ar>
2026-09-23 15:06:05 -0300

    Cambios varios

---
+++ src/components/forms/selectItemse.ts
@@ -0,0 +1,31 @@
+interface Option {
+    value: string;
+    label: string;
+}
+
+export const OptionsItems: Option[] = [
+    {
+        value: "erp",
+        label: "ERP SYS4",
+    },
+    {
+        value: "mes",
+        label: "MES SYS4",
+    },
+    {
+        value: "netsuite",
+        label: "Oracle NetSuite",
+    },
+    {
+        value: "jd-edwards",
+        label: "Oracle JD Edwards",
+    },
+    {
+        value: "consultoria",
+        label: "Consultoría",
+    },
+    {
+        value: "no-se",
+        label: "No sé todavía",
+    },
+];
